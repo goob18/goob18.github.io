0ee1c7731c5cf67576f39c492d85fddcd6c2f8b1
goob18 <guh.huh.egg2@gmail.com>
2025-05-27 13:09:27 -0400

    asdfasdfasdf

--- live.js
+++ live.js
@@ -36,12 +36,12 @@ window.labels = {
     };
     b ? (window.CLOSURE_BASE_PATH = "/javascript/closure/", window.loadStylesheets = function() {
         window.h5CssList.forEach(r)
-    }, p(n + "/lasagna-parse.js"), p(CLOSURE_BASE_PATH + "base.js"), p("/i18n/input/javascript/deps.js"), p("/video/youtube/src/web/javascript/deps-runfiles.js"), p(n + "/deps.js"), p(n + "/js/base_initializer.js"), p(n + "/js/initializer.js"), p(n + "/css-list.js"), q("loadStylesheets()")) : e ? (window.CLOSURE_NO_DEPS = !0, r(n + "/app-prod.css"), p(n + "/app-concat-bundle.js")) : (r(n + "/app-prod.css"), p(n + "/app-prod.js"), (k || l || m) && p(window.environment.player_url));
+    }, p("/lasagna-parse.js"), p(CLOSURE_BASE_PATH + "base.js"), p("/i18n/input/javascript/deps.js"), p("/video/youtube/src/web/javascript/deps-runfiles.js"), p("/deps.js"), p("/js/base_initializer.js"), p("/js/initializer.js"), p("/css-list.js"), q("loadStylesheets()")) : e ? (window.CLOSURE_NO_DEPS = !0, r("/app-prod.css"), p("/app-concat-bundle.js")) : (r("/app-prod.css"), p("/app-prod.js"), (k || l || m) && p(window.environment.player_url));
     window.checkBrokenLabel = function() {
         "undefined" == typeof yt && h && (window.location.href = window.location.href.replace(/([?&])label=[^&]+&?/, "$1stick=0&"))
     };
     q("checkBrokenLabel()");
     f && (window.environment.player_url = e || b ? "/video/youtube/src/web/javascript/debug-tv-player-en_US.js" : "/video/youtube/src/web/javascript/tv-player-en_US.js");
-    g && (e || b ? p(n + "/modules/media-diagnostics-debug.js") : p(n + "/modules/media-diagnostics.js"));
-    q("initializeOrRedirect('" + n + "');");
+    g && (e || b ? p("/modules/media-diagnostics-debug.js") : p("/modules/media-diagnostics.js"));
+    q("initializeOrRedirect('" + "');");
 })();
